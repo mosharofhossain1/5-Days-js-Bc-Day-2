217e4bbd48bbe2467c3efcd3423e9a02dff22ba1
Mosharof <md5470415@gmail.com>
2024-03-26 11:40:54 +0600

    date method practic complate

--- unite-4.js
+++ unite-4.js
@@ -1,3 +1,33 @@
 // Unite Foure:-
 
-console.log('unite Foure is comming')
+console.log('Today Exploring JS Date Object');
+
+let currentDate = new Date();
+// console.log(currentDate);
+
+let day = currentDate.getDate();
+console.log(day);
+
+let month = currentDate.getMonth();
+console.log(month);
+
+let fullyear = currentDate.getFullYear();
+console.log(fullyear);
+
+let houre = currentDate.getHours();
+console.log(houre);
+
+let minite = currentDate.getMinutes();
+console.log(minite);
+
+let second = currentDate.getSeconds();
+console.log(second);
+
+
+
+
+
+
+
+
+
